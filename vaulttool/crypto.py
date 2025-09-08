133c057c898b5f685fc6ee67b8b0f9840258de6d
Josef Fuchs <josef.fuchs@j-fuchs.at>
2025-09-08 05:01:02 +0200

    Start vaulttool as script

--- vaulttool/crypto.py
+++ vaulttool/crypto.py
@@ -3,7 +3,8 @@ import subprocess
 def encrypt_file(input_path, output_path, key_file, algorithm, openssl_path="openssl"):
     cmd = [
         openssl_path, "enc", f"-{algorithm}",
-        "-salt", "-in", input_path,
+        "-salt", "-pbkdf2",  # Use PBKDF2 for secure key derivation
+        "-in", input_path,
         "-out", output_path,
         "-pass", f"file:{key_file}"
     ]
